fe39ea22c243335a412900ca74ede950b43d9b78
Alexander <mypost@example.com>
2022-12-18 14:41:44 +0300

    15

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿//task 15
+
+Console.WriteLine("Введите день недели в числовом выражении");
+int WeekDay = Convert.ToInt32(Console.ReadLine());
+
+if (WeekDay % 6 == 0 || WeekDay % 7 == 0){
+    Console.WriteLine($"Этот день недели - выходной");
+}
+else if(WeekDay < 1 || WeekDay > 7){
+    Console.WriteLine("Даже так? Вы планируете изменить полностью систему летоисчисления?");
+}
+else {
+    Console.WriteLine("Этот день не является выходным");
+}
